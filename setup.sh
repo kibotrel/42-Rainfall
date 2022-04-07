06c3d5f56bb3cc4778e70bb7188b0deed6e5a7cd
Kilian BOTREL <botrel.kilian1999@gmail.com>
2022-04-07 14:50:45 +0200

    Added an option for rebuild only one binary

--- setup.sh
+++ setup.sh
@@ -7,7 +7,9 @@ LEVELS=7
 
 mkdir -p binaries
 
-for (( i=0; i<=$LEVELS; i++ ))
+if (( $# == 0))
+then
+  for (( i=0; i<=$LEVELS; i++ ))
   do
     if (( $i == 0 ))
     then
@@ -22,3 +24,17 @@ for (( i=0; i<=$LEVELS; i++ ))
     $SSHPASS -p $PASSWORD scp -P$PORT level$i/source.c level$i@$IP:/tmp/.
     $SSHPASS -p $PASSWORD ssh -p $PORT level$i@$IP "cd /tmp ; $COMPILATION ; rm -f source.c"
   done
+else
+ if (( $1 == 0 ))
+    then
+      PASSWORD='level0'
+    else
+      PASSWORD=$(cat level$(($1 - 1))/flag)
+    fi
+    COMPILATION=$(cat level$1/Ressources/README.md | grep 'gcc' | cut -c5-)
+
+    $SSHPASS -p $PASSWORD scp -P$PORT level$1@$IP:level$1 binaries/.
+    $SSHPASS -p $PASSWORD ssh -p $PORT level$1@$IP 'chmod 777 .'
+    $SSHPASS -p $PASSWORD scp -P$PORT level$1/source.c level$1@$IP:/tmp/.
+    $SSHPASS -p $PASSWORD ssh -p $PORT level$1@$IP "cd /tmp ; $COMPILATION ; rm -f source.c"
+fi
